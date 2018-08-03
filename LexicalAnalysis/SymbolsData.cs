1c2001ccb50375365cedab156bdb55219e046f1f
Mateo Zuluaga Loaiza <mateo.zuluaga@eia.edu.co>
2018-08-03 10:34:12 -0500

    hopla

---
+++ LexicalAnalysis/SymbolsData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LexicalAnalysis
+{
+    public class SymbolData
+    {
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+
+   
+}
